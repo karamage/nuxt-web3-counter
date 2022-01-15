5801177e9df6bf0f22fc24f36152ffe3de763670
karamage <karamage.umauma@gmail.com>
2022-01-15 09:56:22 +0900

    add contract.

---
+++ contracts/SingleNumRegister.sol
@@ -0,0 +1,20 @@
+// contract/SingleNumRegister.sol
+pragma solidity ^0.8.11;
+
+contract SingleNumRegister {
+  struct StoreNumber {
+    address from;
+    uint256 number;
+  }
+
+  StoreNumber[] public storeNumbers;
+
+  function set(uint256 num) public {
+    storeNumbers.push(StoreNumber(msg.sender, num));
+  }
+
+  function get() public view returns (uint256) {
+    uint256 index = storeNumbers.length - 1;
+    return storeNumbers[index].number;
+  }
+}
