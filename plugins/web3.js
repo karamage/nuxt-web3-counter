784a04cf5baf1fe644fd5fa5ea328034b0477e72
karamage <karamage.umauma@gmail.com>
2022-01-16 16:11:18 +0900

    connet netmask.

--- plugins/web3.js
+++ plugins/web3.js
@@ -2,7 +2,27 @@
 import Web3 from "web3"
 
 export default async function(context, inject) {
-  const httpEndpoint = "http://127.0.0.1:7545" // if change it, connect some chain.
-  const web3 = new Web3(new Web3.providers.HttpProvider(httpEndpoint))
+  let web3
+
+  if (
+    typeof window !== 'undefined' &&
+    typeof window.ethereum !== 'undefined'
+  ) {
+    // MetaMask経由でブロックチェーンに接続
+    web3 = new Web3(window.ethereum)
+    window.ethereum.enable().catch((error) => {
+      // User denied account access
+      console.log(error)
+    })
+  } else if (
+    typeof window !== 'undefined' &&
+    typeof window.web3 !== 'undefined'
+  ) {
+    // 説読後
+    web3 = new Web3(window.web3.currentProvider)
+  } else {
+    const httpEndpoint = "http://127.0.0.1:7545" // if change it, connect some chain.
+    web3 = new Web3(new Web3.providers.HttpProvider(httpEndpoint))
+  }
   inject('web3', web3)
 }
