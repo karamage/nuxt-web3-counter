870d909acfdfc57c406cf72b00bb3738c326370e
karamage <karamage.umauma@gmail.com>
2022-01-19 09:07:31 +0900

    tweak truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -18,7 +18,7 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
